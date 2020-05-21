f26a4bb28e2a944c50056a6b5f74307ddfaac8ab
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-05-22 00:07:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CSS Beautifier
+
+Firefox extension that beautifies CSS files open on browser  
+  
+Uses vkBeautify and Highlight.js
+
---
+++ beautifier.js
@@ -0,0 +1,16 @@
+const pre = document.getElementsByTagName('pre')[0];
+css_code = pre.innerText;
+pre.innerText = "";
+
+const code = document.createElement("code");
+code.classList.add("css");
+pre.appendChild(code);
+//code.innerText = css_code;
+
+const source_code = vkbeautify.css(css_code);
+code.innerText = source_code;
+
+hljs.configure({useBR: true});
+hljs.highlightBlock(code);
+
+document.body.style.backgroundColor = window.getComputedStyle(code).backgroundColor;
