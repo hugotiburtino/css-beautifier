455e5c7644848765b0286e312dee688dd239c8fb
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-01 21:56:45 +0200

    Read panel template file and add it to the DOM
    Also fix typo at condition

--- css_beautifier.js
+++ css_beautifier.js
@@ -2,9 +2,34 @@ isRawText = document.body.childNodes.length === 1 &&
         document.body.children[0].nodeName === "PRE" &&
         document.body.children[0].children.length === 0 &&
         document.head.children.length === 1 &&
-        ddocument.head.children[0].rel === "stylesheet"
+        document.head.children[0].rel === "stylesheet"
 
 if (isRawText) {
+
+        let htmlFile = ''
+
+        // https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file 
+        function readPanelTemplate(file)
+        {
+                var rawFile = new XMLHttpRequest();
+                rawFile.open("GET", file, false);
+                rawFile.onreadystatechange = function ()
+                {
+                        if(rawFile.readyState === 4)
+                        {
+                                if(rawFile.status === 200 || rawFile.status == 0)
+                                {
+                                        var allText = rawFile.responseText;
+                                        htmlFile = allText;
+                                }
+                        }
+                }
+                rawFile.send(null);
+        }
+        readPanelTemplate(browser.runtime.getURL("panel.html"));
+        
+        document.body.appendChild(document.createRange()
+                .createContextualFragment(htmlFile))
 
         const pre = document.getElementsByTagName('pre')[0];
         css_code = pre.innerText;
