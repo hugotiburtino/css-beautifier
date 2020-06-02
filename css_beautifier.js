c3caaa46efb4f9fffb5ad3ff95bc8e79bc6d38bc
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-02 15:16:50 +0200

    Wrap the condition of raw text into a function
    And change its name and ocurrence

--- css_beautifier.js
+++ css_beautifier.js
@@ -1,10 +1,18 @@
-isRawText = document.body.childNodes.length === 1 &&
-        document.body.children[0].nodeName === "PRE" &&
-        document.body.children[0].children.length === 0 &&
-        document.head.children.length === 1 &&
-        document.head.children[0].rel === "stylesheet"
 
-if (isRawText) {
+function checkIfPlainText() {
+        
+        if (document.body.childNodes.length === 1 &&
+                document.body.children[0].nodeName === "PRE" &&
+                document.body.children[0].children.length === 0 &&
+                document.head.children.length === 1 &&
+                document.head.children[0].rel === "stylesheet") {
+                        
+                        return true
+                
+                }
+}
+
+if (checkIfPlainText) {
 
         htmlFile = ''
     
