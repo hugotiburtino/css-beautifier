beda631e2df993a85209b0b5d86d9e64d6ed4214
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-02 15:20:07 +0200

    Wrap the creation of the html into a function and call it

--- css_beautifier.js
+++ css_beautifier.js
@@ -12,30 +12,38 @@ function checkIfPlainText() {
                 }
 }
 
-if (checkIfPlainText) {
+function buildPanel() {
 
         htmlFile = ''
-    
+                
         // adapted from https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file 
         readPanelTemplate(file) {
-            var rawFile = new XMLHttpRequest();
-            rawFile.open("GET", file, false);
-            rawFile.onreadystatechange = () => {
+                var rawFile = new XMLHttpRequest();
+                rawFile.open("GET", file, false);
+                rawFile.onreadystatechange = () => {
                 if(rawFile.readyState === 4) {
                         if(rawFile.status === 200 || rawFile.status == 0) {
                                 const allText = rawFile.responseText;
                                 htmlFile = allText;
                         }
                 }
-            }
-            rawFile.send(null);
         }
-    
+        rawFile.send(null);
+        }
+        
         readPanelTemplate(browser.runtime.getURL("resources/panel.html"));
-            
+        
         document.body.appendChild(document.createRange()
                 .createContextualFragment(htmlFile))
+        
+}
 
+function beautify() {
+        
+        if (checkIfPlainText) {
+                
+        buildPanel();
+        
         const pre = document.getElementsByTagName('pre')[0];
         const css_code = pre.innerText;
         pre.innerText = "";
@@ -43,21 +51,21 @@ if (checkIfPlainText) {
         const code = document.createElement("code");
         code.classList.add("css");
         pre.appendChild(code);
-    
+        
         const codeColumn = document.getElementById('value');
         codeColumn.appendChild(pre);
 
         const rawContainer = document.createElement('pre');
         rawContainer.innerText = css_code;
-
+        
         const rawPanel = document.getElementById('rawdata-panel')
         rawPanel.appendChild(rawContainer);
 
         const rawTab = document.getElementById('rawdata-tab')
         const beautyTab = document.getElementById('json-tab')
-
+        
         rawTab.addEventListener('click', () =>
-                {
+        {
                         rawTab.parentNode.classList.add('is-active')
                         beautyTab.parentNode.classList.remove('is-active')
                         rawPanel.style.visibility = "visible"
@@ -65,28 +73,29 @@ if (checkIfPlainText) {
                         document.getElementById('json-panel')
                                 .style.visibility = "hidden"
                         document.getElementById('json-panel')
-                                .style.height = "0px"
+                        .style.height = "0px"
                 }
-        )
-        
+                )
+                
 
-        beautyTab.addEventListener('click', () =>
+                beautyTab.addEventListener('click', () =>
                 {
                         rawTab.parentNode.classList.remove('is-active')
                         beautyTab.parentNode.classList.add('is-active')
                         rawPanel.style.visibility = "hidden"
                         rawPanel.style.height = "0px"
                         document.getElementById('json-panel')
-                                .style.visibility = "visible"
+                        .style.visibility = "visible"
                         document.getElementById('json-panel')
-                                .style.height = "100%"
+                        .style.height = "100%"
                 }
         )
-
+        
         const source_code = css_beautify(css_code);
         code.innerHTML = source_code;
-
+        
         hljs.highlightBlock(code);
+        
+        }
 
 }
-
