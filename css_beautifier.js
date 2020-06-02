8352fa119945e94d5b0f08c3b1f3c7b8f39699e4
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-02 20:12:25 +0200

    Add rest of appropriate cmds into buildPanel()
    And minor changes for readability

--- css_beautifier.js
+++ css_beautifier.js
@@ -31,75 +31,70 @@ function readPanelTemplate(file) {
         return html
 }
 
-function buildPanel() {
+function buildPanel(pre, css_code) {
 
-        const htmlFile = readPanelTemplate(browser.runtime.getURL("resources/panel.html"));
+        const htmlFile = 
+                readPanelTemplate(browser.runtime.getURL("resources/panel.html"));
         
         document.body.appendChild(document.createRange()
-                .createContextualFragment(htmlFile))
-        
-}
+                .createContextualFragment(htmlFile));
 
-function beautify() {
-        
-    if (checkIfPlainText) {
-                
-        buildPanel();
-
-        const pre = document.getElementsByTagName('pre')[0];
-        const css_code = pre.innerText;
-        pre.innerText = "";
-
-        const code = document.createElement("code");
-        code.classList.add("css");
-        pre.appendChild(code);
-        
         const codeColumn = document.getElementById('value');
         codeColumn.appendChild(pre);
-
+                
         const rawContainer = document.createElement('pre');
         rawContainer.innerText = css_code;
-        
-        const rawPanel = document.getElementById('rawdata-panel')
+
+        const rawPanel = document.getElementById('rawdata-panel');
         rawPanel.appendChild(rawContainer);
 
-        const rawTab = document.getElementById('rawdata-tab')
-        const beautyTab = document.getElementById('json-tab')
+        const rawTab = document.getElementById('rawdata-tab');
+        const beautyTab = document.getElementById('json-tab');
         
-        rawTab.addEventListener('click', () =>
-        {
-                        rawTab.parentNode.classList.add('is-active')
-                        beautyTab.parentNode.classList.remove('is-active')
-                        rawPanel.style.visibility = "visible"
-                        rawPanel.style.height = "100%"
-                        document.getElementById('json-panel')
-                                .style.visibility = "hidden"
-                        document.getElementById('json-panel')
-                        .style.height = "0px"
-                }
-                )
+        rawTab.addEventListener('click', () =>   {
+                rawTab.parentNode.classList.add('is-active');
+                beautyTab.parentNode.classList.remove('is-active');
+                rawPanel.style.visibility = "visible";
+                rawPanel.style.height = "100%";
+                document.getElementById('json-panel')
+                        .style.visibility = "hidden";
+                document.getElementById('json-panel')
+                        .style.height = "0px";
+        });
                 
-
-                beautyTab.addEventListener('click', () =>
-                {
-                        rawTab.parentNode.classList.remove('is-active')
-                        beautyTab.parentNode.classList.add('is-active')
-                        rawPanel.style.visibility = "hidden"
-                        rawPanel.style.height = "0px"
-                        document.getElementById('json-panel')
-                        .style.visibility = "visible"
-                        document.getElementById('json-panel')
-                        .style.height = "100%"
-                }
-        )
         
-        const source_code = css_beautify(css_code);
-        code.innerHTML = source_code;
+        beautyTab.addEventListener('click', () => {
+                rawTab.parentNode.classList.remove('is-active');
+                beautyTab.parentNode.classList.add('is-active');
+                rawPanel.style.visibility = "hidden";
+                rawPanel.style.height = "0px";
+                document.getElementById('json-panel')
+                        .style.visibility = "visible";
+                document.getElementById('json-panel')
+                        .style.height = "100%";
+        });
         
-        hljs.highlightBlock(code);
-        
-        }
-
 }
 
-beautify()
+(function beautify() {
+        
+        if (checkIfPlainText) {
+                
+                
+                const pre = document.getElementsByTagName('pre')[0];
+                const css_code = pre.innerText;
+                pre.innerText = "";
+                
+                const code = document.createElement("code");
+                code.classList.add("css");
+                pre.appendChild(code);
+                
+                const source_code = css_beautify(css_code);
+                code.innerHTML = source_code;
+                
+                hljs.highlightBlock(code);
+                
+                buildPanel(pre, css_code);
+        }
+
+})()
