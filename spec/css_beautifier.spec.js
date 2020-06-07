83d435e6c7453fc74eb43976d6a82cb9e0bb5d76
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-07 23:52:29 +0200

    Change global to vm context
    Since the module.exports made the extension crash, a work around
    is to create a virtual machine and make the css_beautifier.js run
    in it. That way we don't have to change the source code only to
    test it, what is better.

--- spec/css_beautifier.spec.js
+++ spec/css_beautifier.spec.js
@@ -1,25 +1,35 @@
+const fs = require('fs');
+const vm = require('vm');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 describe("CSS Beautifier", function() {
     
+    const css_beautifier = fs.readFileSync('src/css_beautifier.js')
+    const context = { }
+    vm.runInContext(css_beautifier, vm.createContext(context))
+    const { checkIfPlainText } = context
+
+
     describe("checkIfPlainText", function() {
         
-        const { checkIfPlainText } = require("../src/css_beautifier")
+        //const { checkIfPlainText } = require("../src/css_beautifier");
 
         it("should exclude files without element in head that has relation of stylesheet", function() {
             const { document } = 
                new JSDOM('<html><head></head><body><pre>Code without the stylesheet in head</pre></body></html>').window;
-            global.document = document;
-            expect(checkIfPlainText()).toBe(false)
+            context.document = document;
+            expect(checkIfPlainText()).toBe(false);
         });
 
         it("should exclude files with more than just pre tag in body", function() {
             const { document } = 
                new JSDOM('<html><head></head><body><pre>Code with more than just a pre tag</pre><p>See my awesome code above</></body></html>').window;
-            global.document = document;
-            expect(checkIfPlainText()).toBe(false)
+            context.document = document;
+            expect(checkIfPlainText()).toBe(false);
         });
+
+        // TODO test other situations
         
     });
 });
