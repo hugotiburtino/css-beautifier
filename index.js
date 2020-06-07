e5d265c4a8a658db6179390c8ab62d7512db961f
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-07 23:16:37 +0200

    Add copyright and basic description of extension

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2020 Hugo Tiburtino.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-----------------------------------
+
+CSS Beautifier - Firefox Extension
+Author: Hugo B. Tiburtino 
+
+Firefox extension that beautifies CSS files
+
+Get it on [Firefox Browser ADD-ONS](https://addons.mozilla.org/en-US/firefox/addon/css-beautifier/)!
+
+---
+
+Automatically beautifies and colors a CSS file open in a separate tab on Firefox
+
+## Build with
+* [JS Beautifier](https://github.com/beautify-web/js-beautify) 1.11.0 | MIT
+* [Highlight.js](https://github.com/highlightjs/highlight.js) 10.0.3 | BSD 3
+* Json Panel of the Firefox built-in Json Viewer | MLP 2.0 (Adapted)
+
+## Credits
+Icon made by [Smashicons](https://www.flaticon.com/authors/smashicons) available at flaticon
+
+*/
 
 beautify()
