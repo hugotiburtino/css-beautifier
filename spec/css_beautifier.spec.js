06a89da3ef8c457499c5b64245b4e1da1a9ddaae
Hugo B Tiburtino <hugotiburtino@protonmail.com>
2020-06-08 00:16:04 +0200

    Complete test situation of checkIfPlainText
    Minor wording changes and delete comment.

--- spec/css_beautifier.spec.js
+++ spec/css_beautifier.spec.js
@@ -10,26 +10,49 @@ describe("CSS Beautifier", function() {
     vm.runInContext(css_beautifier, vm.createContext(context))
     const { checkIfPlainText } = context
 
-
     describe("checkIfPlainText", function() {
         
-        //const { checkIfPlainText } = require("../src/css_beautifier");
-
         it("should exclude files without element in head that has relation of stylesheet", function() {
             const { document } = 
-               new JSDOM('<html><head></head><body><pre>Code without the stylesheet in head</pre></body></html>').window;
+               new JSDOM('<html><head></head><body><pre>Html without the stylesheet in head</pre></body></html>').window;
             context.document = document;
             expect(checkIfPlainText()).toBe(false);
         });
 
-        it("should exclude files with more than just pre tag in body", function() {
+        it("should exclude files with more elements in head than a relation of stylesheet", function() {
             const { document } = 
-               new JSDOM('<html><head></head><body><pre>Code with more than just a pre tag</pre><p>See my awesome code above</></body></html>').window;
+               new JSDOM('<html><head><link rel="stylesheet" href="resource://content-accessible/plaintext.css"><title>CSS code</title></head><body><pre>Html with more than one element in head</pre></body></html>').window;
             context.document = document;
             expect(checkIfPlainText()).toBe(false);
         });
 
-        // TODO test other situations
-        
+        it("should exclude files with more than just one pre tag in body", function() {
+            const { document } = 
+               new JSDOM('<html><head><link rel="stylesheet" href="resource://content-accessible/plaintext.css"></head><body><pre>Html with more than just a pre tag</pre><pre>See my awesome code above</pre></body></html>').window;
+            context.document = document;
+            expect(checkIfPlainText()).toBe(false);
+        });
+
+        it("should exclude files with nodes within the pre tag", function() {
+            const { document } = 
+               new JSDOM('<html><head><link rel="stylesheet" href="resource://content-accessible/plaintext.css"></head><body><pre>Html with <em>more nodes here</em> than just a pre tag</pre></body></html>').window;
+            context.document = document;
+            expect(checkIfPlainText()).toBe(false);
+        });
+
+        it("should exclude files no pre tag", function() {
+            const { document } = 
+               new JSDOM('<html><head><link rel="stylesheet" href="resource://content-accessible/plaintext.css"></head><body><code>Html with no pre tag</code></body></html>').window;
+            context.document = document;
+            expect(checkIfPlainText()).toBe(false);
+        });
+
+        it("should accept files that match the usual way of rendering code as plain text at FF", function() {
+            const { document } = 
+               new JSDOM('<html><head><link rel="stylesheet" href="resource://content-accessible/plaintext.css"></head><body><pre>a { color: black }</pre></body></html>').window;
+            context.document = document;
+            expect(checkIfPlainText()).toBe(true);
+        });
+
     });
 });
